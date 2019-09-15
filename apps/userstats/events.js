349e868f711ae3046dfc7006c52ea0990678b049
Jacob Dent <jdent@tacyarg.com>
2019-09-15 09:38:22 -0400

    userstats

--- apps/userstats/events.js
+++ apps/userstats/events.js
@@ -1,16 +1,6 @@
-const Database = require('../models')
-const highland = require('highland')
-const { Cache, Defaults } = require('../libs/stats')
-
-// process events creating stats by userid.
-
-module.exports = async config => {
-  const { events, userstats, users, items, cases } = await Database(
-    config.rethink
-  )
+module.exports = ({ users, items, cases, statsCache }) => {
 
   let totalEvents = 0
-  const statsCache = Cache(Defaults.user)
   const boxes = {}
 
   const handleCaseEvent = async event => {
@@ -73,79 +63,13 @@ module.exports = async config => {
   }
 
   // route the event type to the correct topic processor.
-  async function handleEvent(event) {
-    console.log('handleEvent:', ++totalEvents, event.id)
+  return async event => {
+    console.log('handleEvent:', ++totalEvents, event.type, event.id)
     switch (event.type) {
       case 'case-opened':
         return handleCaseEvent(event)
       case 'trade-offers':
         return handleTradeEvent(event)
     }
-  }
-
-  // buffer changes
-  const realtimeBuffer = highland()
-
-  // cache all realtime events while we resume.
-  events
-    .changes()
-    .map(row => row.new_val)
-    .compact()
-    .pipe(realtimeBuffer)
-
-  // pause, resume service from memory
-  const last = await userstats
-    .streamSorted()
-    .map(statsCache.set)
-    .errors(err => {
-      console.error('Error Resuming cache:', err)
-      process.exit(1)
-    })
-    .last()
-    .toPromise(Promise)
-
-  // process all events from last mutation to now.
-  await events
-    .streamFrom(last.updated)
-    .filter(row => row.item.price)
-    .map(handleEvent)
-    .flatMap(highland)
-    .errors(err => {
-      console.error(err)
-      process.exit(1)
-    })
-    .toPromise(Promise)
-
-  // dump cache to db
-  await statsCache
-    .highland()
-    .map(userstats.upsert)
-    .flatMap(highland)
-    .errors(err => {
-      console.error(err)
-      process.exit(1)
-    })
-    .last()
-    .toPromise(Promise)
-
-  // process realtime events forever.
-  realtimeBuffer
-    .map(handleEvent)
-    .flatMap(highland)
-    .map(userstats.upsert)
-    .flatMap(highland)
-    .errors(err => {
-      console.error(err)
-      // process.exit(1)
-    })
-    .resume()
-
-  return {
-    async openStream() {
-      return realtimeBuffer.toPromise(Promise)
-    },
-    async list() {
-      return statsCache.list()
-    },
   }
 }
